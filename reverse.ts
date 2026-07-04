7252c64082b9f048d54a038cd627c88415c54250
vilasgiri2045 <vilasgiri2045@gmail.com>
2026-07-04 14:19:47 +0530

    Implement function to reverse a number

---
+++ reverse.ts
@@ -0,0 +1,13 @@
+function reverseNumber(num: number): number {
+    let reverse = 0;
+
+    while (num > 0) {
+        let digit = num % 10;
+        reverse = reverse * 10 + digit;
+        num = Math.floor(num / 10);
+    }
+
+    return reverse;
+}
+
+console.log(reverseNumber(12345));
